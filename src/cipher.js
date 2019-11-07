a655c05af61885d679680ee56d79ed9c47a68553
bianca vicencio <bianquita@Air-de-monita.iptv.microsoft.com>
2019-11-07 00:05:05 -0300

    primer avance proyecto 1 Laboratoria

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,23 @@
 window.cipher = {
+  
   encode: () => {
     /* Acá va tu código */
-    
+    var letra = 'ABC';
+    var letraAscii;
+    var posicionCifrada;
+    var letraCifrada;
+    var offSet = 3;
+    letraAscii = letra.charCodeAt(0);
+    console.log(letraAscii);
+
+    //aplicar formula
+    posicionCifrada = (letraAscii - 65 + offSet) % 26 + 65;
+
+    //pasar ascii cifrado a su letra correspondiente
+    letraCifrada = String.fromCharCode(posicionCifrada);
+
+    //mostrar
+    console.log('La letra cifrada es : ' + letraCifrada);
   },
   decode: () => {
     /* Acá va tu código */
